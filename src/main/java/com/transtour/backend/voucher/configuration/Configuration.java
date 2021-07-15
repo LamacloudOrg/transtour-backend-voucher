caf1878a4e3c0509b5095d1d505cf542b2c9544e
charlyla <cnlaffitte@gmail.com>
2021-07-14 23:03:53 -0300

    add new application properties with db strings

--- src/main/java/com/transtour/backend/voucher/configuration/Configuration.java
+++ src/main/java/com/transtour/backend/voucher/configuration/Configuration.java
@@ -4,6 +4,7 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
@@ -12,6 +13,12 @@ import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 @org.springframework.context.annotation.Configuration
 public class Configuration extends AbstractMongoClientConfiguration {
 
+    @Value("${mongo.string.connection}")
+    private String driverName;
+
+    @Value("${db.name}")
+    private String dbName;
+
     @Bean
     MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
         return new MongoTransactionManager(dbFactory);
@@ -19,12 +26,12 @@ public class Configuration extends AbstractMongoClientConfiguration {
 
     @Override
     protected String getDatabaseName() {
-        return "transtour";
+        return dbName;
     }
 
     @Override
     public MongoClient mongoClient() {
-        final ConnectionString connectionString = new ConnectionString("mongodb://mongo-host:27017/transtour");
+        final ConnectionString connectionString = new ConnectionString(driverName);
         final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
