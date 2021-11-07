f2dd9e0a2fd76394c3ff9d6334732ba9e4f5beed
charlyla <cnlaffitte@gmail.com>
2021-11-07 20:13:49 -0300

    add new jasypt encript to properties application

--- src/main/java/com/transtour/backend/voucher/VoucherApplication.java
+++ src/main/java/com/transtour/backend/voucher/VoucherApplication.java
@@ -1,7 +1,7 @@
 package com.transtour.backend.voucher;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableEncryptableProperties
 public class VoucherApplication {
 
 	public static void main(String... args) {
