720db42e584292402815fcfac9dfb21e33b147c5
charlyla <cnlaffitte@gmail.com>
2022-09-15 21:08:02 -0300

    change method signature apiCall

--- src/main/java/com/transtour/backend/voucher/repository/ICompanyRepository.java
+++ src/main/java/com/transtour/backend/voucher/repository/ICompanyRepository.java
@@ -2,10 +2,8 @@ package com.transtour.backend.voucher.repository;
 
 import com.transtour.backend.voucher.model.Company;
 import feign.Param;
-import feign.RequestLine;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,6 +16,7 @@ public interface ICompanyRepository {
     Company getCompany(@PathVariable String fullName);
     */
 
-    @RequestLine("GET /v1/company?fullName={fullName}")
+  //  @RequestLine("GET /v1/company?fullName={fullName}")
+    @RequestMapping(method= RequestMethod.GET,value = "/v1/company?fullName={fullName}")
     Company getCompany(@Param(value = "fullName") String fullName);
 }
