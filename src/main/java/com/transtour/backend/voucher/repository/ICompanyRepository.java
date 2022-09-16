f19ed7d80bedeec6e2d64777d82d362eb589c281
charlyla <cnlaffitte@gmail.com>
2022-09-15 21:57:07 -0300

    bug fix

--- src/main/java/com/transtour/backend/voucher/repository/ICompanyRepository.java
+++ src/main/java/com/transtour/backend/voucher/repository/ICompanyRepository.java
@@ -1,9 +1,9 @@
 package com.transtour.backend.voucher.repository;
 
 import com.transtour.backend.voucher.model.Company;
-import feign.Param;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,12 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(name = "SPRING-CLOUD-USER-API")
 public interface ICompanyRepository {
 
-    /*
-    @RequestMapping(method= RequestMethod.GET,value = "/v1/company?fullName")
-    Company getCompany(@PathVariable String fullName);
-    */
-
-  //  @RequestLine("GET /v1/company?fullName={fullName}")
-    @RequestMapping(method= RequestMethod.GET,value = "/v1/company/search?fullName={fullName}")
-    Company getCompany(@Param(value = "fullName") String fullName);
+  @RequestMapping(method = RequestMethod.GET, value = "/v1/company/{fullName}")
+  Company getCompany(@PathVariable String fullName);
 }
