dab35bb8a55883e3bb4120feb804d367c99c9b9e
charlyla <cnlaffitte@gmail.com>
2022-09-18 20:57:20 -0300

    change type double to string in email type

--- src/main/java/com/transtour/backend/voucher/model/Company.java
+++ src/main/java/com/transtour/backend/voucher/model/Company.java
@@ -11,22 +11,8 @@ public class Company {
     private String fullName;
     private String nickName;
     private String cuit;
-    private Double email;
+    private String email;
     private String phone;
     private Double whitingTimeAmount;
     private Double dispositionTimeAmount;
-
-    @Override
-    public String toString() {
-        return "Company{" +
-                "id=" + id +
-                ", fullName='" + fullName + '\'' +
-                ", nickName='" + nickName + '\'' +
-                ", cuit='" + cuit + '\'' +
-                ", email=" + email +
-                ", phone='" + phone + '\'' +
-                ", whitingTimeAmount=" + whitingTimeAmount +
-                ", dispositionTimeAmount=" + dispositionTimeAmount +
-                '}';
-    }
 }
