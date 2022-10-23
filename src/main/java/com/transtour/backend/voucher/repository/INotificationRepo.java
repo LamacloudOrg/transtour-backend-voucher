5501d4a122f731a0da0ff80ebc36198cd59020de
charlyla <cnlaffitte@gmail.com>
2022-10-23 20:48:58 -0300

    change name intance for repository

--- src/main/java/com/transtour/backend/voucher/repository/INotificationRepo.java
+++ src/main/java/com/transtour/backend/voucher/repository/INotificationRepo.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Qualifier("NotificationRepo")
-@FeignClient(name = "SPRING-CLOUD-NOTIFICATION-API")
+@FeignClient(name = "SPRING-CLOUD-NOTIFICATION")
 public interface INotificationRepo {
 
     @RequestMapping(method = RequestMethod.POST, value = "/v1/notification/sendPdfToPassenger")
